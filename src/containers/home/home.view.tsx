fa0af9de015a04619fc626dfbbdda8ffbfe8f8a8
hsn6 <hsn6@tuta.io>
2021-12-29 21:31:41 +0330

    Add more user info to the home screen

--- src/containers/home/home.view.tsx
+++ src/containers/home/home.view.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { MouseEventHandler } from "react";
-import { Avatar, Stack } from "@mui/material";
+import { Avatar, Box, Chip, Stack } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import type { User } from "../../store/auth/auth.types";
 
@@ -25,11 +25,26 @@ export const HomeView = ({
       {user !== undefined && (
         <>
           <Avatar src={user.avatar} />
-          <div>Hey {user.firstName}!</div>
+          <Box>Hey {user.firstName}!</Box>
+          <Box>
+            email: {user.email}{" "}
+            <Chip
+              label={user.emailVerified ? "verified" : "unverified"}
+              color={user.emailVerified ? "success" : "warning"}
+            />
+          </Box>
+          <Box>
+            phone number: {user.phoneNumber}{" "}
+            <Chip
+              label={user.phoneNumberVerified ? "verified" : "unverified"}
+              color={user.phoneNumberVerified ? "success" : "warning"}
+            />
+          </Box>
           <LoadingButton
             variant={"contained"}
             onClick={onClickLogout}
             loading={isLoadingLogout}
+            color={"error"}
           >
             Logout
           </LoadingButton>
